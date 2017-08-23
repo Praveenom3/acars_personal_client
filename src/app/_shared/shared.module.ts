3127f288482320f1cec05f05a4153f7155a5b147
vinodoptionm <vinod@optionmatrix.com>
2017-08-23 12:58:12 +0530

    Update shared.module.ts

--- src/app/_shared/shared.module.ts
+++ src/app/_shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PhoneMaskDirective } from "app/directives/phone-mask.directive";
+import { inputFilterDirective } from "app/_directives/input-filter.directive";
 
 @NgModule({
   imports: [
     CommonModule,
   ],
-  declarations: [PhoneMaskDirective],
-     exports: [PhoneMaskDirective],
+  declarations: [PhoneMaskDirective,inputFilterDirective],
+     exports: [PhoneMaskDirective,inputFilterDirective],
 })
 export class SharedModule { }
