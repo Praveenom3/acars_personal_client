d87558a38218b1418626b565c165df7e8e22adf9
HARISH\Harish <harish@optionmatrix.com>
2017-08-26 12:10:03 +0530

    Get labels in Element Master

--- src/app/_services/_element-master.service.ts
+++ src/app/_services/_element-master.service.ts
@@ -5,7 +5,7 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/catch';
 
-import {GlobalService} from './_global.service';
+import { GlobalService } from './_global.service';
 
 @Injectable()
 
@@ -13,30 +13,36 @@ export class ElementMasterService {
 
     private _elementMasterUrl = this._globalService.apiHost + '/element-master';
 
-   constructor(private _globalService: GlobalService,
-                private _http: Http) {
-         
+    constructor(private _globalService: GlobalService,
+        private _http: Http) {
+
     }
 
-    public updateElement(ElementData) :Observable<any> {
-        console.log(ElementData);
+    public updateElement(ElementData): Observable<any> {
         return this._http.put(
-                    this._elementMasterUrl +'/'+ElementData.element_id,
-                     JSON.stringify({
-                        "element_label": ElementData.element_label
-                       }),
-                    {headers: this._globalService.getHeaders()}
-             ).map(response => response.json())
+            this._elementMasterUrl + '/' + ElementData.element_id,
+            JSON.stringify({
+                "element_label": ElementData.element_label
+            }),
+            { headers: this._globalService.getHeaders() }
+        ).map(response => response.json())
             .catch(this._globalService.handleError);
     }
 
-   public getElementData() :Observable<any> {
+    public getElementData(): Observable<any> {
         return this._http.get(
-                    this._elementMasterUrl,
-                    {headers:  this._globalService.getHeaders()}
-             ) .map((response: Response) => response.json().data)
+            this._elementMasterUrl,
+            { headers: this._globalService.getHeaders() }
+        ).map((response: Response) => response.json().data)
             .catch(this._globalService.handleError);
     }
 
+    public getLabels(section_id,product_id): Observable<any> {
+        return this._http.get(
+            this._elementMasterUrl+'/get-labels/'+section_id+'/'+product_id,
+            { headers: this._globalService.getHeaders() }
+        ).map((response: Response) => response.json().data)
+            .catch(this._globalService.handleError);
+    }
 
 }
