f71309b24e2697480a30b0b5991c5c2ccd9f98db
Naveen <naveen@optionmatrix.com>
2017-08-30 16:40:55 +0530

    plan classes

--- src/app/client/employer-info/plan-classes/add-plan-class/coverage-type/coverage-type.component.ts
+++ src/app/client/employer-info/plan-classes/add-plan-class/coverage-type/coverage-type.component.ts
@@ -1,20 +1,247 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
+import { PlanClassesService } from "app/_services/_plan-classes.service";
+import { ElementMasterService } from "app/_services/_element-master.service";
+import { ToastrService } from "ngx-toastr";
+import { GlobalService } from "app/_services/_global.service";
 
 @Component({
   selector: 'app-coverage-type',
   templateUrl: './coverage-type.component.html',
   styleUrls: ['./coverage-type.component.css']
 })
+
 export class CoverageTypeComponent implements OnInit {
-  company: string;
-  product: string;
-  constructor(route: ActivatedRoute) { 
+  client_id: any;
+  purchase_id: any;
+  companyDetails: any;
+  coverageTypeData: { plan_class_id: number; company_id: number; plan_class_number: string; plan_class_name: string; plan_offer_type: string; plan_type_doh: string; employee_medical_plan: string; coverageType: string[]; waitingType: string[]; created_at: string; created_by: string; updated_at: string; updated_by: string; };
+  label: string;
+    
+    company: string;
+    product: string;
+    company_id: any;
+    product_id: any;
+    encodedId: string = null;
+    id: number = null;
+
+    section_id: any = 9;
+    public labels: any[] = [];
+
+    _errorMessage: string;
+    
+  constructor(route: ActivatedRoute,
+    private router: Router,
+    private toastrService: ToastrService,
+    public globalService: GlobalService,
+    private _elementMasterService: ElementMasterService,
+    private planClassesService: PlanClassesService) {
+
+    this.product_id = globalService.decode(route.snapshot.params['product']);
+    this.company_id = globalService.decode(route.snapshot.params['company']);
     this.product = route.snapshot.params['product'];
     this.company = route.snapshot.params['company'];
+
+    this.encodedId = route.snapshot.params['encodedId'];
+    
+    if(this.encodedId){
+      this.id = globalService.decode(this.encodedId);
+    }
+    
   }
 
   ngOnInit() {
+    this.coverageTypeData = this.createNewCoverageType();
+    this.ElementLabelsList();
+    this.getCompany();
+    
+    if(this.id){
+      this.getCoverageTypeData();
+    }else{
+
+      this.planClassesService.getMaxPlanClassNumber()
+      .subscribe((data) => {
+        this.coverageTypeData.plan_class_number = data.maxPlanClassNumber;
+      },
+      error => { this._errorMessage = error.data }
+      );
+    }
   }
+
+  getCompany() {
+    let companyDet = this.globalService.getCompany();
+    let products = JSON.parse(localStorage.getItem('productsAndClients'));
+    let productYear = products[this.product_id]['applicable_year'];
+    if (companyDet) {
+      this.companyDetails = JSON.parse(companyDet);
+      this.companyDetails.productYear = productYear;
+      this.purchase_id = this.companyDetails.purchase_id;
+      this.client_id = this.companyDetails.client_id;
+    }
+  }
+
+  createNewCoverageType() {
+    // Create a new Brand
+    let coverageType = {
+      plan_class_id: 0,
+      company_id: 0,
+      plan_class_number: '',
+      plan_class_name: '',
+      plan_offer_type: '',
+      plan_type_doh: '',
+      employee_medical_plan: '',
+      coverageType: ["", "", "", "", "", "", "", "", "", "", "", "", ""],
+      waitingType: ["", "", "", "", "", "", "", "", "", "", "", "", ""],
+      created_at: '',
+      created_by: '',
+      updated_at: '',
+      updated_by: ''
+    }
+    return coverageType;
+  }
+
+  
+  changeSelectType(type, value) {
+    if (value != 1) {
+        if(type = 'coverageType'){
+            this.coverageTypeData.coverageType = ["", "", "", "", "", "", "", "", "", "", "", "", ""];
+        }else{
+            this.coverageTypeData.waitingType = ["", "", "", "", "", "", "", "", "", "", "", "", ""];
+        }
+    }
+  }
+
+  /*getting data from service*/
+  private getCoverageTypeData() {
+    this.planClassesService.getPlanClass(this.id)
+      .subscribe((planClassData) => {
+        if (planClassData.planClassInformation) {
+          
+          this.coverageTypeData.plan_class_id = planClassData.planClassInformation.plan_class_id;
+          this.coverageTypeData.plan_class_number = planClassData.planClassInformation.plan_class_number;
+          this.coverageTypeData.plan_class_name = planClassData.planClassInformation.plan_class_name;
+          this.coverageTypeData.plan_offer_type = planClassData.planClassInformation.plan_offer_type;
+          this.coverageTypeData.plan_type_doh = planClassData.planClassInformation.plan_type_doh;
+          this.coverageTypeData.employee_medical_plan = planClassData.planClassInformation.employee_medical_plan;
+
+          let planOfferTypeYear = planClassData.planClassInformation.planOfferTypeYear;
+
+          if(this.coverageTypeData.plan_offer_type != '1' ){
+              if(this.coverageTypeData.plan_offer_type == '5' ){
+                
+                this.coverageTypeData.coverageType = this.AssignMonths(planOfferTypeYear, 1);
+                this.coverageTypeData.waitingType = this.AssignMonths(planOfferTypeYear, 3);
+              }else if(this.coverageTypeData.plan_type_doh == '9'){
+                this.coverageTypeData.waitingType = this.AssignMonths(planOfferTypeYear, 2);
+              }
+          }
+        }
+      },
+      error => { this._errorMessage = error.data }
+      );
+  }
+
+  private AssignMonths(data, combination_type){
+    
+    let MonthFields: any[] = [];
+    if (data.length > 0) {
+   
+      data.forEach(eachSelectedMonth => {
+        
+        if(eachSelectedMonth.combination_type == combination_type){
+          MonthFields[eachSelectedMonth.plan_month] = eachSelectedMonth.plan_month_value;
+        }
+      });
+      for (let i = 1; i <= 12; i++) {
+        if (!MonthFields[i]) {
+          MonthFields[i] = "";
+        }
+      }
+      
+    } else {
+      MonthFields = ["", "", "", "", "", "", "", "", "", "", "", "", ""];
+    }
+    return MonthFields;
+  }
+
+  /*getting labels from service*/
+  private ElementLabelsList() {
+    this._elementMasterService.getLabels(this.section_id, this.product_id)
+      .subscribe((labels) => {
+        for (let label of labels) {
+          this.label = label.element_serial_id + ' ' + label.element_label;
+          this.labels.push(this.label);
+        }
+      },
+      error => { this._errorMessage = error.data }
+      );
+  }
+
+  /*on submit sending form data to service.It is for both add and update*/
+  public onSubmit(param) {
+
+    if(this.coverageTypeData.plan_class_name.length == 0){
+      this.toastrService.success('Plan class name cannot be empty.');
+      return false;
+    }
+
+    let customCoverageTypeArray = [];
+    if (this.coverageTypeData.coverageType.length > 0) {
+      this.coverageTypeData.coverageType.forEach((eachSelectedMonth, index) => {
+        if (eachSelectedMonth) {
+          customCoverageTypeArray[index] = eachSelectedMonth;
+        }
+      });
+    }
+
+    let customWaitingTypeArray = [];
+    if (this.coverageTypeData.waitingType.length > 0) {
+      this.coverageTypeData.waitingType.forEach((eachSelectedMonth, index) => {
+        if (eachSelectedMonth) {
+          customWaitingTypeArray[index] = eachSelectedMonth;
+        }
+      });
+    }
+    
+         this.coverageTypeData.coverageType = customCoverageTypeArray;
+         this.coverageTypeData.waitingType = customWaitingTypeArray;
+         this.coverageTypeData['company_id'] = this.company_id;
+         
+        if (this.coverageTypeData.plan_class_id > 0) {
+          this.planClassesService.updateCoverageType(this.coverageTypeData).subscribe(
+            result => {
+              if (result.success) {
+                this.encodedId = this.globalService.encode(result.data.PlanClassCoverageTypeInformation.plan_class_id);
+                if (param == "exit") {
+                  this.router.navigate(['client/' + this.product + '/' + this.company]);
+                } else {
+                  this.router.navigate(['client/' + this.product + '/' + this.company + '/employer-info/plan-classes/plan-class/'+this.encodedId+ '/coverage-offered']);
+                }
+                this.toastrService.success('Coverage Type Information record added succesfully.');
+              } else {
+                this._errorMessage = 'Not Added.';
+              }
+            },
+            error => {
+            });
+        } else {
+          this.planClassesService.createCoverageType(this.coverageTypeData).subscribe(
+            result => {
+              if (result.success) {
+                if (param == "exit") {
+                  this.router.navigate(['client/' + this.product + '/' + this.company]);
+                } else {
+                  this.router.navigate(['client/' + this.product + '/' + this.company + '/employer-info/plan-classes/plan-class/'+this.encodedId+ '/coverage-offered']);
+                }
+                this.toastrService.success('Coverage Type Information record updated succesfully.');
+              } else {
+                this._errorMessage = 'Not Updated.';
+              }
+            },
+            error => {
+            });
+        }
+    
+    }
 
 }
--- src/app/client/employer-info/plan-classes/add-plan-class/employee-contributions/employee-contributions.component.ts
+++ src/app/client/employer-info/plan-classes/add-plan-class/employee-contributions/employee-contributions.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
+import { PlanClassesService } from "app/_services/_plan-classes.service";
+import { ElementMasterService } from "app/_services/_element-master.service";
+import { ToastrService } from "ngx-toastr";
+import { GlobalService } from "app/_services/_global.service";
 
 @Component({
   selector: 'app-employee-contributions',
@@ -7,14 +11,150 @@ import { ActivatedRoute } from "@angular/router";
   styleUrls: ['./employee-contributions.component.css']
 })
 export class EmployeeContributionsComponent implements OnInit {
+  client_id: any;
+  purchase_id: any;
+  companyDetails: any;
+  employeeContributionData: { plan_class_id: number; safe_harbor: string; employee_plan_contribution: string; premiums: string[]; created_at: string; created_by: string; updated_at: string; updated_by: string; };
+
+  label: string;
+  
   company: string;
   product: string;
-  constructor(route: ActivatedRoute) { 
-    this.product = route.snapshot.params['product'];
-    this.company = route.snapshot.params['company'];
+  company_id: any;
+  product_id: any;
+  encodedId: string = null;
+  id: number = null;
+
+  section_id: any = 11;
+  public labels: any[] = [];
+
+  _errorMessage: string;
+
+  constructor(route: ActivatedRoute,
+    private router: Router,
+    private toastrService: ToastrService,
+    public globalService: GlobalService,
+    private _elementMasterService: ElementMasterService,
+    private planClassesService: PlanClassesService) {
+
+      this.product_id = globalService.decode(route.snapshot.params['product']);
+      this.company_id = globalService.decode(route.snapshot.params['company']);
+      this.product = route.snapshot.params['product'];
+      this.company = route.snapshot.params['company'];
+  
+      this.encodedId = route.snapshot.params['encodedId'];
+      
+      if(this.encodedId){
+        this.id = globalService.decode(this.encodedId);
+      }
   }
 
   ngOnInit() {
+    this.employeeContributionData = this.createNewEmployeeContribution();
+    this.ElementLabelsList();
+    this.getCompany();
+    
+    if(this.id){
+      this.getEmployeeContributionData();
+    }
+  }
+  
+  getCompany() {
+    let companyDet = this.globalService.getCompany();
+    let products = JSON.parse(localStorage.getItem('productsAndClients'));
+    let productYear = products[this.product_id]['applicable_year'];
+    if (companyDet) {
+      this.companyDetails = JSON.parse(companyDet);
+      this.companyDetails.productYear = productYear;
+      this.purchase_id = this.companyDetails.purchase_id;
+      this.client_id = this.companyDetails.client_id;
+    }
   }
 
+  
+  createNewEmployeeContribution() {
+    // Create a new Brand
+    let employeeContribution = {
+      plan_class_id: 0,
+      safe_harbor: '',
+      employee_plan_contribution: '',
+      premiums: ["", "", "", "", "", "", "", "", "", "", "", "", ""],
+      created_at: '',
+      created_by: '',
+      updated_at: '',
+      updated_by: ''
+    }
+    return employeeContribution;
+  }
+
+  /*getting labels from service*/
+  private ElementLabelsList() {
+    this._elementMasterService.getLabels(this.section_id, this.product_id)
+      .subscribe((labels) => {
+        for (let label of labels) {
+          this.label = label.element_serial_id + ' ' + label.element_label;
+          this.labels.push(this.label);
+        }
+      },
+      error => { this._errorMessage = error.data }
+      );
+  }
+
+  /*getting data from service*/
+  private getEmployeeContributionData() {
+
+    this.planClassesService.getEmployeeContribution(this.id)
+      .subscribe((employeeContributionInfo) => {
+       
+        if (employeeContributionInfo.employeeContribution) {
+          
+          this.employeeContributionData.safe_harbor = employeeContributionInfo.employeeContribution.safe_harbor;
+          this.employeeContributionData.employee_plan_contribution = employeeContributionInfo.employeeContribution.employee_plan_contribution;
+          let premiums = employeeContributionInfo.employeeContribution.empContributionPremiums;
+          
+          let MonthFields: any[] = [];
+          if (premiums.length > 0) {         
+            premiums.forEach(eachSelectedMonth => {              
+                MonthFields[eachSelectedMonth.premium_month] = eachSelectedMonth.premium_value;
+            });
+            for (let i = 1; i <= 12; i++) {
+              if (!MonthFields[i]) {
+                MonthFields[i] = "";
+              }
+            }            
+          } else {
+            MonthFields = ["", "", "", "", "", "", "", "", "", "", "", "", ""];
+          }
+          this.employeeContributionData.premiums = MonthFields;
+        }
+      },
+      error => { this._errorMessage = error.data }
+      );
+  }
+
+  public onBlur(element){
+    if(element.target.value){
+      element.target.value = parseFloat(element.target.value).toFixed(2);
+    }
+  }
+/*on submit sending form data to service.It is for both add and update*/
+public onSubmit(param) {
+  
+      this.planClassesService.createOrUpdateEmployeeContribution(this.id, this.employeeContributionData).subscribe(
+        result => {
+          if (result.success) {
+            if (param == "exit") {
+              this.router.navigate(['client/' + this.product + '/' + this.company]);
+            } else {
+              this.router.navigate(['client/' + this.product + '/' + this.company + '/employer-info/payroll']);
+            }
+            this.toastrService.success('Coverage Type Information record added succesfully.');
+          } else {
+            this._errorMessage = 'Not Added.';
+          }
+        },
+        error => {
+        });
+           
+       }
 }
