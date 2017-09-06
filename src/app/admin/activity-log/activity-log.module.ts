fdde914806c3960d48cd3f155ddaceb31dcca38c
vinodoptionm <vinod@optionmatrix.com>
2017-09-06 16:08:36 +0530

    Update activity-log.module.ts

--- src/app/admin/activity-log/activity-log.module.ts
+++ src/app/admin/activity-log/activity-log.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import {ActivityRoutingModule} from './activity.routing';
+import {ActivityLogRoutingModule} from './activity-log.routing';
 import {ActivityLogComponent} from './activity-log.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    ActivityRoutingModule
+    ActivityLogRoutingModule
   ],
   declarations: [ActivityLogComponent]
 })
