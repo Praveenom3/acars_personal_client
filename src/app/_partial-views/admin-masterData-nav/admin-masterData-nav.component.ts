924717f96e3feee7b18a8b88e5a02accfd202988
Naveen <naveen@optionmatrix.com>
2017-09-19 18:28:14 +0530

    master data layout redesign

---
+++ src/app/_partial-views/admin-masterData-nav/admin-masterData-nav.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-masterData-nav',
+  templateUrl: './admin-masterData-nav.component.html',
+  styleUrls: ['./admin-masterData-nav.component.css']
+})
+export class AdminMasterDataNavComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
