1d38274f3ad62424b13a1f5e83f71e34675e4469
SatishBanda <satish@optionmatrix.com>
2017-09-20 18:06:10 +0530

    Client Observations

--- src/app/_partial-views/client-reporting-band/client-reporting-band.component.ts
+++ src/app/_partial-views/client-reporting-band/client-reporting-band.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
 import { GlobalService } from "app/_services/_global.service";
-
+import { ModalDirective } from "ngx-bootstrap";
 @Component({
   selector: 'client-reporting-band',
   templateUrl: './client-reporting-band.component.html',
   styleUrls: ['./client-reporting-band.component.css']
 })
 export class ClientReportingBandComponent implements OnInit {
+  @ViewChild('companyUploadDataFile') public companyUploadDataFile: ModalDirective;
+
   companyData: any;
   company: string;
   product: string;
@@ -18,6 +20,25 @@ export class ClientReportingBandComponent implements OnInit {
 
   ngOnInit() {
     this.companyData = JSON.parse(this._globalService.getCompany());
+  }
+
+  /**
+   * 
+   */
+  public uploadDataFiles() {
+    let today: any = new Date().getTime();
+    var uploadDate: any = new Date("2017-10-15").getTime();
+    if (parseInt(today) < parseInt(uploadDate)) {
+        this.companyUploadDataFile.show();
+    } 
+  }
+
+
+  /**
+   * 
+   */
+  public closeUploadDataModal() {
+    this.companyUploadDataFile.hide();
   }
 
 }
