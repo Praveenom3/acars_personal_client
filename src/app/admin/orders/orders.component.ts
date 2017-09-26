3638106d69a7ac68fe8fdd357d631730427d2f42
Naveen <naveen@optionmatrix.com>
2017-09-26 14:16:25 +0530

    add/update purchases-product fix, phone format, status msg

--- src/app/admin/orders/orders.component.ts
+++ src/app/admin/orders/orders.component.ts
@@ -492,6 +492,8 @@ export class OrdersComponent implements OnInit {
                 this.getSelectableProducts('', '', 'clientAddPurchase');
             }
 
+            this.checkNewAndUpdatePurchasesList();
+
             this._addPurchaseFormSubmitted = false;
 
             this.addPurchaseModal.show();
@@ -524,6 +526,9 @@ export class OrdersComponent implements OnInit {
                 this.getSelectableProducts(this._updateClientForm.value.client_id, '', 'clientAddPurchase');
                 this.getSelectableProducts('', this._updatePurchaseForm.value.product_id, 'clientUpdatePurchaseBeforeSubmit');
             }
+            
+            this.checkNewAndUpdatePurchasesList(this._updateClientForm.value.client_id);
+            
             this.updatePurchaseModal.show();
         }
     }
@@ -586,6 +591,64 @@ export class OrdersComponent implements OnInit {
             }
         }
         return "NA";
+    }
+
+    public checkNewAndUpdatePurchasesList(client_id?){
+        if (this.newPurchases.length > 0) {
+            this.newPurchases.forEach((newProductElement, index) => {
+                let product_full_name = this.getItemName('product', newProductElement.product_id);
+                
+                this.selectableProducts.forEach((selectableProductElement, index) => {
+                    Globals.products_keywords.forEach(product_key => {
+                        if (product_full_name.indexOf(product_key) !== -1) {
+                            if (selectableProductElement.product_full_name.indexOf(product_key) !== -1) {
+                                this.selectableProducts[index].is_deleted = 1;
+                            }
+                        }
+                    });
+                });
+            });
+        }
+        if (this.updatePurchases.length > 0) {
+
+            this.updatePurchases.forEach((updateProductElement, index) => {
+                //change the previously selected value of the update purchase to not deleted status
+                this.clientsTableData.forEach(clientx => {
+                    if(clientx.client_id == client_id){
+                        clientx.clientPurchases.forEach(purchase => {
+                            if (purchase.purchase_id == updateProductElement.product_id) {
+                                let product_full_name = this.getItemName('product', updateProductElement.product_id);
+                                
+                                Globals.products_keywords.forEach(product_key => {
+                                    if (product_full_name.indexOf(product_key) !== -1) {
+                                        this.availableProducts.forEach((availableProductElement, index) => {
+                                            if (availableProductElement.product_full_name.indexOf(product_key) !== -1) {
+                                                if (availableProductElement.is_deleted == 0) {
+                                                    this.selectableProducts[index].is_deleted = 0;
+                                                }
+                                            }
+                                        });
+                                    }
+                                });
+                            }
+                        });
+                    }
+                    
+                });
+                //assigning the newly selected product to deleted status
+                let product_full_name = this.getItemName('product', updateProductElement.product_id);
+                
+                this.selectableProducts.forEach((selectableProductElement, index) => {
+                    Globals.products_keywords.forEach(product_key => {
+                        if (product_full_name.indexOf(product_key) !== -1) {
+                            if (selectableProductElement.product_full_name.indexOf(product_key) !== -1) {
+                                this.selectableProducts[index].is_deleted = 1;
+                            }
+                        }
+                    });
+                });
+            });
+        }
     }
 
     public onSubmit(form) {
@@ -1049,19 +1112,9 @@ export class OrdersComponent implements OnInit {
                 this.totalPurchases.splice(totalIndex, 1);
             }
 
-            //updating the selectable products
-            let product_full_name = this.getItemName('product', item.product_id);
+            //updating the orders with selectable products
+            this.orders = this.getOrders();
             
-            this.selectableProducts.forEach((selectableProductElement, index) => {
-                Globals.products_keywords.forEach(product_key => {
-                    if (product_full_name.indexOf(product_key) !== -1) {
-                        if (selectableProductElement.product_full_name.indexOf(product_key) !== -1) {
-                            this.selectableProducts[index].is_deleted = 0;
-                        }
-                    }
-                });
-            });
-
             //Scenario 2 : Purchase is Old
             //  }else if(item.hasOwnProperty('is_new_purchase') && item.is_new_purchase == 0){
         } else {
@@ -1093,18 +1146,9 @@ export class OrdersComponent implements OnInit {
                                     if (totalIndex !== -1) {
                                         this.totalPurchases.splice(totalIndex, 1);
                                     }
-                                    //updating the selectable products
-                                    let product_full_name = this.getItemName('product', item.product_id);
-                                    
-                                    this.selectableProducts.forEach((selectableProductElement, index) => {
-                                        Globals.products_keywords.forEach(product_key => {
-                                            if (product_full_name.indexOf(product_key) !== -1) {
-                                                if (selectableProductElement.product_full_name.indexOf(product_key) !== -1) {
-                                                    this.selectableProducts[index].is_deleted = 0;
-                                                }
-                                            }
-                                        });
-                                    });
+                                    //updating the orders with selectable products
+                                    this.orders = this.getOrders();
+
                                     this.toastrService.success('Purchase deleted succesfully.');
                                 } else {
                                     this.toastrService.success('Trouble in removing the purchase. Please try later.');
