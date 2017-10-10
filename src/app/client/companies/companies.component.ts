c859596ddbafe7f3cc2a1a693738c1403aae5c23
Satish Goud <Satish@optionmatrix.com>
2017-10-10 13:13:20 +0530

    Copy Company Functionality

--- src/app/client/companies/companies.component.ts
+++ src/app/client/companies/companies.component.ts
@@ -24,6 +24,7 @@ export class CompaniesComponent implements OnInit {
   @ViewChild('companyModal') public companyModal: ModalDirective;
   @ViewChild('companyUserModal') public companyUserModal: ModalDirective;
   @ViewChild('companyUploadDataFile') public companyUploadDataFile: ModalDirective;
+  @ViewChild('copyCompanyModal') public copyCompanyModal: ModalDirective;
 
   public modalTitle: string;
   public companyUserModalTitle: string;
@@ -48,6 +49,10 @@ export class CompaniesComponent implements OnInit {
   public userType: any;
   public mask = [/\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/]
   public phoneNumberMask = ['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]
+
+  companyDropdownList = [];
+  selectedCompanies = [];
+  companyDropdownSettings = {};
 
   constructor(public route: ActivatedRoute,
     private _formBuilder: FormBuilder,
@@ -267,10 +272,6 @@ export class CompaniesComponent implements OnInit {
           this.resetCompanyInfo(result);
           this._submitted = false;
           this.toastrService.success('Company Details Updated Successfully.');
-          let companies: any[] = this.clientDashBoardService.companies;
-          companies.push(result.data);
-
-          this.clientDashBoardService.companies = companies;
         } else {
           this._errorMessage = 'Record not Updated';
           this._submitted = false;
@@ -605,7 +606,80 @@ export class CompaniesComponent implements OnInit {
     this._submitted = false;
     this.companyModal.show();
   }
+  /**
+   * 
+   */
+  copyCompany() {
+    let companiesList = this.clientDashBoardService.companies;
+    let companies = [];
 
+    companiesList.forEach(element => {
+      if (element.company_ein && element.company_id != this.clientDashBoardService.company.company_id) {
+        let company = [];
+        company['id'] = element.company_id;
+        company['itemName'] = element.company_name
+        companies.push(company);
+      }
+    });
+    this.companyDropdownList = companies;
+
+    this.companyDropdownSettings = {
+      singleSelection: false,
+      text: "Select Companies",
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All',
+      enableSearchFilter: true,
+      classes: "myclass custom-class",
+      searchPlaceholderText: "Search Companies",
+      enableCheckAll: true
+    };
+
+    this.copyCompanyModal.show();
+  }
+  onItemSelect(item: any) {
+
+  }
+  OnItemDeSelect(item: any) {
+
+  }
+  onSelectAll(items: any) {
+
+  }
+  onDeSelectAll(items: any) {
+
+  }
+  /**
+   * 
+   */
+  closeCopyCompanyModal() {
+    this.selectedCompanies = [];
+    this.copyCompanyModal.hide();
+  }
+  /**
+   * 
+   */
+  copyCompanySubmit() {
+    let companyIds: any[] = [];
+    this.selectedCompanies.forEach((element) => {
+      companyIds.push(element.id);
+    });
+    let data = {
+      'toCompanyIds': companyIds,
+      'fromCompanyId': this.clientDashBoardService.company.company_id
+    };
+    this.companyUserService.copyCompanyDetails(data).subscribe(
+      result => {
+        if (result.success) {
+          this.closeCopyCompanyModal();
+          this.toastrService.success('Company Details copied successfully');
+        } else {
+          this.toastrService.error('Failed to copy Company Details');
+        }
+      },
+      error => {
+        this.toastrService.error('Failed to copy Company Details');
+      });
+  }
   setFormErrorsOnChange(form, formErrors, data?: any) {
     if (!form) { return; }
     for (let field in formErrors) {
